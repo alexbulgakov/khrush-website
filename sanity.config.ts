283235ccb05f799a23c8144b9068cbaffadbc18e
alexbulgakov <contact.bulgakov@gmail.com>
2024-02-14 03:24:10 +0700

    feat(sanity): add title

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,25 +1,17 @@
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...index]]\page.tsx` route
- */
+import { visionTool } from "@sanity/vision";
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import { apiVersion, dataset, projectId } from "./sanity/env";
+import { schema } from "./sanity/schema";
 
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schema'
+const title = process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE || "Храш - админ";
 
 export default defineConfig({
-  basePath: '/studio',
+  basePath: "/studio",
   projectId,
   dataset,
-  // Add and edit the content schema in the './sanity/schema' folder
+  title,
   schema,
-  plugins: [
-    structureTool(),
-    // Vision is a tool that lets you query your content with GROQ in the studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
-  ],
-})
+  plugins: [structureTool(), visionTool({ defaultApiVersion: apiVersion })],
+});
