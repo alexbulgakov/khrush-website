1d357b4585ef8f1f504b02987e43341bb46c30ef
alexbulgakov <contact.bulgakov@gmail.com>
2024-03-19 21:56:43 +0700

    refactor: delete files

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import withPlaiceholder from '@plaiceholder/next'
-
 /** @type {import('next').NextConfig} */
 const config = {
   // webpack(config, { isServer }) {
@@ -39,4 +37,4 @@ const config = {
   },
 }
 
-export default withPlaiceholder(config)
+export default config
